e4e6e777fdfd1e6bcf078f8fb6c89f93b6a62e37
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-11-26 17:50:43 +0530

    Vdom re-render and more staf with hooks and state with daynamic apdate

--- Project-Based-Learing-Food-App/src/Components/BodyComponets.jsx
+++ Project-Based-Learing-Food-App/src/Components/BodyComponets.jsx
@@ -1,8 +1,11 @@
 import RestroCard from "./RestroCard"
-import restorentData from "../restorentData"
-
+import { useState } from "react";
+import restorentData from "../data/restorentData"
 
 const BodyComponents = () => {
+  const arr = useState(restorentData?.data?.restaurants ?? []);
+  const [resData , setRestorentData] = arr;
+  console.log(resData);
   return (
     <div className="body-container">
       <div className="flex justify-center mt-5">
@@ -11,13 +14,16 @@ const BodyComponents = () => {
           placeholder="Search for food items..."
           className="border-2 border-gray-300 p-3 rounded-md w-1/2"
         />
-        <button className="bg-blue-500 text-white rounded-md ml-2 w-20">
+        <button className="bg-blue-500 text-white rounded-md ml-2 w-20 hover:cursor-pointer" onClick={() => {
+        let  res= resData.filter((f) => f.rating > 4.5);
+          setRestorentData(res);
+        }} >
           Search
         </button>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-7 justify-items-center">
-        {restorentData.data.restaurants.map((restorent) => (
+        {resData.map((restorent) => (
           <RestroCard key={restorent.id} resData={restorent} />
         ))}
       </div>
